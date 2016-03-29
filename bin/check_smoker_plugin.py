6b70c3f1231d694a772a4e633fc74c7cc2580f8c
Ondřej Štumpf <ondrej.stumpf@gooddata.com>
2016-03-29 10:04:32 +0200

    FEATURE: DSS-2594 improve usability of check_smoker_plugin

--- bin/check_smoker_plugin.py
+++ bin/check_smoker_plugin.py
@@ -15,58 +15,109 @@ from smoker.client import Client
 import smoker.util.nagios as nagios
 import smoker.logger
 
+UNKNOWN = "UNKNOWN"
+ERROR = "ERROR"
+WARN = "WARN"
+OK = "OK"
 
 # Don't log into console, use only syslog
 smoker.logger.init(syslog=True, console=False)
 lg = logging.getLogger('check_smoker_plugin')
 
 parser = argparse.ArgumentParser(description='Smoker Nagios plugin')
-parser.add_argument('-f', '--force', dest='force', action='store_true', help="Force plugins run (otherwise just print last results)")
-parser.add_argument('plugin', help="Plugin to check")
+parser.add_argument('-f', '--force', dest='force', action='store_true',
+                    help="Force plugins run (otherwise just print last results)")
+parser.add_argument('--category', default=None, help='The category of tests to run')
+parser.add_argument('--component', default=None, help='The tests component to run')
+parser.add_argument('--health', default=False, dest='health', action='store_true', help="Run only healthchecks")
+parser.add_argument('plugin', default=None, nargs='*', help="Plugin to check")
 args = parser.parse_args()
 
-def ParseIssue(plugin_result):
+
+def parse_issue(plugin_result):
     out = "smoke test: %s " % plugin_result['name']
     for component in plugin_result['lastResult']['componentResults']:
-        if component['componentResult']['status'] != 'OK':
-            out += "%s: %s; " % ( component['componentResult']['name'], component['componentResult']['status'] )
+        if component['componentResult']['status'] != OK:
+            out += "%s: %s; " % ( component['componentResult']['name'], component['componentResult']['status'])
     if len(out) > 1024:
         out = "smoke test: %s returns huge error output, see smoker logs for details" % plugin_result['name']
     return out
 
+
 def main():
     client = Client(['localhost'])
-    plugins = client.get_plugins([[args.plugin]])
+    plugins = None
+
+    if args.plugin:
+        if args.category or args.component or args.health:
+            lg.warn("Plugins specified by name, ignoring --category, --component and --health")
+
+        plugins = client.get_plugins([args.plugin])
+    elif args.category or args.component or args.health:
+        filter = []
+        if args.category:
+            filter.append({'key': 'Category', 'value': args.category})
+        if args.component:
+            filter.append({'key': 'Component', 'value': args.component})
+        if args.health:
+            filter.append({'key': 'Type', 'value': 'healthCheck'})
+        plugins = client.get_plugins(filter)
+    else:
+        nagios.exit_unknown("invalid startup configuration - neither plugin nor --category nor --component "
+                            "nor --health specified")
 
     # No plugin found
     if not plugins:
-        nagios.exit_unknown("plugin %s not found" % args.plugin)
+        if args.plugin:
+            nagios.exit_unknown("plugin %s not found" % args.plugin)
+        else:
+            nagios.exit_ok("no plugin found by category %s and component %s, health: %s" %
+                           (args.category, args.component, args.health))
 
     if args.force:
         plugins = client.force_run(plugins, progress=False)
 
+    status_methods_pairs = [(ERROR, nagios.exit_critical), (UNKNOWN, nagios.exit_unknown),
+                            (WARN, nagios.exit_warning), (OK, nagios.exit_ok)]
+
     # Manage plugin result. We can't return much data to Nagios, so just say if it's alright or not
+    results = dict((s, []) for s, _ in status_methods_pairs)
+
     for plugin in plugins.values()[0]['plugins'].itervalues():
+        plugin_name = plugin['name']
         if not plugin['lastResult']:
-            # No result?! -> UNKNOWN
-            nagios.exit_unknown("plugin has no last result")
-        elif plugin['lastResult']['status'] == 'OK':
-            try:
-                nagios.exit_ok(plugin['lastResult']['messages']['ok'][0])
-            except (KeyError, IndexError, TypeError):
-                nagios.exit_ok("smoke test %s succeeded at %s" % ( plugin['name'], plugin['lastResult']['lastRun']) )
-        elif plugin['lastResult']['status'] == 'WARN':
-            try:
-                nagios.exit_warning(plugin['lastResult']['messages']['warn'][0])
-            except (KeyError, IndexError, TypeError):
-                nagios.exit_warning(ParseIssue(plugin))
-        elif plugin['lastResult']['status'] == 'ERROR':
-            try:
-                nagios.exit_critical(plugin['lastResult']['messages']['error'][0])
-            except (KeyError, IndexError, TypeError):
-                nagios.exit_critical(ParseIssue(plugin))
+            results[UNKNOWN].append({'name': plugin_name, 'message': "plugin has no last result"})
         else:
-            nagios.exit_unknown("unknown status %s at %s" % (plugin['lastResult']['status'], plugin['lastResult']['lastRun']))
+            last_status = plugin['lastResult']['status']
+
+            if last_status in [ERROR, WARN, OK]:
+                try:
+                    results[last_status].append({'name': plugin_name,
+                                                 'message': plugin['lastResult']['messages'][last_status.lower()][0]})
+                except (KeyError, IndexError, TypeError):
+                    if last_status == OK:
+                        results[last_status].append(
+                            {'name': plugin_name,
+                             'message': "smoke test %s succeeded at %s" %
+                                        (plugin['name'], plugin['lastResult']['lastRun'])})
+                    else:
+                        results[last_status].append({'name': plugin_name, 'message': parse_issue(plugin)})
+            else:
+                results[UNKNOWN].append({'name': plugin_name,
+                                         'message': "unknown status %s at %s" %
+                                                    (plugin['lastResult']['status'], plugin['lastResult']['lastRun'])})
+
+    for status, exit_method in status_methods_pairs:
+        if results[status]:
+            if len(plugins.values()[0]['plugins']) == 1:
+                # if only one plugin has been executed, do not print summary
+                exit_method(results[status][0]['message'])
+            else:
+                summary = ', '.join(["%s: %s" % (s, len(results[s])) for s, _ in status_methods_pairs if results[s]])
+                messages = ['\n'.join(["%s - %s - %s" % (s, item['name'], item['message']) for item in list])
+                            for s, list in results.iteritems() if list]
+                exit_method("%s\n%s" % (summary, '\n'.join(messages)))
+
 
 if __name__ == '__main__':
     try:
